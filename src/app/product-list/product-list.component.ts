4de3f726c5bf5aad25b9c4a37ee59b6b3ea84cf6
Veranono29 <gescuder@hotmail.com>
2021-11-24 22:00:28 +0100

    prueba relocate

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -5,16 +5,16 @@ import { products } from '../products';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
   products = products;
 
   share() {
     window.alert('The product has been shared!');
+    window.location.replace('https://angular.io');
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
